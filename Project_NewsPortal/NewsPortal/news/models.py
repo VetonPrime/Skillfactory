4fc61402531aee03d67b361b277b19629839ffc1
Veton <Veton2004@mail.ru>
2023-04-17 22:08:35 +0300

    ИТОГОВОЕ ЗАДАНИЕ 8.6.2 (HW-03)

--- Project_NewsPortal/NewsPortal/news/models.py
+++ Project_NewsPortal/NewsPortal/news/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 article = 'AR'
 news = 'NE'
@@ -89,3 +91,18 @@ class Comment(models.Model):
     def dislike(self):
         self.rating -= 1
         self.save()
+
+
+class BaseRegisterForm(UserCreationForm):
+    email = forms.EmailField(label="Email")
+    first_name = forms.CharField(label="Имя")
+    last_name = forms.CharField(label="Фамилия")
+
+    class Meta:
+        model = User
+        fields = ("username",
+                  "first_name",
+                  "last_name",
+                  "email",
+                  "password1",
+                  "password2", )
--- Project_NewsPortal/NewsPortal/news/urls.py
+++ Project_NewsPortal/NewsPortal/news/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import (
-   NewsList, NewDetail, NewsListSearch, NewCreate, NewEdit, NewDelete, ArticleCreate, ArticleEdit, ArticleDelete
+   NewsList, NewDetail, NewsListSearch, NewCreate, NewEdit, NewDelete, ArticleCreate, ArticleEdit, ArticleDelete,
+   BaseRegisterView, upgrade_me
 )
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
    path('', NewsList.as_view(), name='news_list'),
@@ -13,4 +15,14 @@ urlpatterns = [
    path('article/create/', ArticleCreate.as_view(), name='articles_create'),
    path('article/<int:pk>/edit/', ArticleEdit.as_view(), name='articles_edit'),
    path('article/<int:pk>/delete/', ArticleDelete.as_view(), name='articles_delete'),
+   path('login/',
+        LoginView.as_view(template_name='news/login.html'),
+        name='login'),
+   path('logout/',
+        LogoutView.as_view(template_name='news/logout.html'),
+        name='logout'),
+   path('signup/',
+        BaseRegisterView.as_view(template_name='news/signup.html'),
+        name='signup'),
+   path('upgrade/', upgrade_me, name='upgrade'),
 ]
